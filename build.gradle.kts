5c6649d1d9da5ba57bd8aeb7df8d0cdc61a51f29
Geo <jbandrie@gmail.com>
2026-08-08 19:03:35 +0800

    Remove android configuration from build.gradle.kts
    Removed android configuration block from build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,3 @@ plugins {
     id("com.google.gms.google-services") version "4.4.1" apply false
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
 }
-
-android {
-    namespace = "com.example.bibleversealarm" // O kung ano man ang iyong package name
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.example.bibleversealarm"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-        
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-    
-    // Iba pang configurations...
-}
