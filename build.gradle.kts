e19e228f75d55190e0dfaedb06d5e40f4fa599d3
Geo <jbandrie@gmail.com>
2026-08-08 18:48:01 +0800

    Refactor build.gradle.kts for plugin and SDK settings
    Updated plugin versions and added Android configuration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,24 @@
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.bibleversealarm" // O kung ano man ang iyong package name
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.bibleversealarm"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    
+    // Iba pang configurations...
 }
