363de91f232fa6e69ab51e5ce1848ed169203580
Geo <jbandrie@gmail.com>
2026-08-08 21:24:09 +0800

    build: upgrade Gradle to 9.3.1 and refactor config
    Updated the build system to use version catalogs, upgraded to Gradle 9.3.1, cleaned up project configuration, and streamlined CI workflows.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.google.devtools.ksp) apply false
+  alias(libs.plugins.roborazzi) apply false
+  alias(libs.plugins.secrets) apply false
+  alias(libs.plugins.google.services) apply false
 }
