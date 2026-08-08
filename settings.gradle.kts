5466450b34995f082fe809ee72aa37a16a343c7d
Geo <jbandrie@gmail.com>
2026-08-08 18:24:32 +0800

    Register KSP plugin in settings.gradle.kts
    Added KSP plugin registration to the Gradle settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,8 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    // Idagdag ang com.google.devtools.ksp dito para ma-register ito nang maaga sa project repositories
+    id("com.google.devtools.ksp") version "2.2.10-1.0.30" apply false
 }
 
 dependencyResolutionManagement {
